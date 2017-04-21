ca0e4208aa681ab6276a41516c9ab03f33c35fd8
anasilviacs <anascsilva@vib-ugent.be>
2017-04-21 12:15:35 +0200

    Add TITLE to pin file; Fixed indexing error and lazy parser

--- percid2mgftitle.py
+++ percid2mgftitle.py
@@ -21,13 +21,15 @@ for i in range(len(doc['MzIdentML']['DataCollection']['AnalysisData']['SpectrumI
         for j in range(len(doc['MzIdentML']['DataCollection']['AnalysisData']['SpectrumIdentificationList']['SpectrumIdentificationResult'][i]['SpectrumIdentificationItem'])):
             spectrum = doc['MzIdentML']['DataCollection']['AnalysisData']['SpectrumIdentificationList']['SpectrumIdentificationResult'][i]
             hit = spectrum['SpectrumIdentificationItem'][j]
-            perc_id = hit['@id'] + '_' + hit['@chargeState'] + '_' + hit['@rank']
+            # perc_id = hit['@id'] + '_' + hit['@chargeState'] + '_' + hit['@rank']
+            perc_id = hit['@id']
             title = spectrum['cvParam']['@value']
             mapper[perc_id] = title
     else:
         spectrum = doc['MzIdentML']['DataCollection']['AnalysisData']['SpectrumIdentificationList']['SpectrumIdentificationResult'][i]
         hit = spectrum['SpectrumIdentificationItem']
-        perc_id = hit['@id'] + '_' + hit['@chargeState'] + '_' + hit['@rank']
+        # perc_id = hit['@id'] + '_' + hit['@chargeState'] + '_' + hit['@rank']
+        perc_id = hit['@id']
         title = spectrum['cvParam']['@value']
         mapper[perc_id] = title
 
@@ -42,7 +44,9 @@ def lazy_pin_parser(path):
     f = open(path)
     rows = f.readlines()
     for i, row in enumerate(rows):
-        if i == 0: data = pd.DataFrame(columns=row.split('\t')); n_rows = len(row.split('\t'))
+        if i == 0:
+            data = pd.DataFrame(columns=[r.translate({'"': None}).rstrip('\n') for r in row.split('\t')])
+            n_rows = len(row.split('\t'))
         elif i == 1: continue # row 1 is initial direction
         else:
             r = row.split('\t')
@@ -52,3 +56,15 @@ def lazy_pin_parser(path):
             tmp.append('|'.join(r[n_rows-1:]).rstrip('\n'))
             data.loc[i-1] = tmp
     return data
+
+pin = lazy_pin_parser(args.pin)
+# Adding mgf "TITLE" column
+pin['TITLE'] = [None] * len(pin)
+for i in range(1, len(pin)+1): # because index starts at 1
+    k = '_'.join(pin.loc[i, 'SpecId'].split('_')[-6:-3])
+    if k in mapper.keys():
+        pin.loc[i, 'TITLE'] = mapper[k]
+    else:
+        continue
+
+pin.to_csv(args.pin, sep='\t', index=False)
