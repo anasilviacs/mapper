9bc7c62b2f026b6eb59470de6650547200e2db8e
anasilviacs <anascsilva@vib-ugent.be>
2017-04-27 11:41:14 +0200

    Added different processing for non-concat searches

--- percid2mgftitle.py
+++ percid2mgftitle.py
@@ -1,39 +1,42 @@
 import argparse
 import xmltodict
 import pandas as pd
+import sys
 
 # Arguments: percolator features, .MzIdentML
 parser = argparse.ArgumentParser(description="Take pin file built with Percolator's msgf2pin and add the 'TITLE' from the mgf file")
-parser.add_argument('-m', dest='mzml', help='Path to mzml file')
+parser.add_argument('-m', dest='mzml', help='Path to single mzml file (concatenated search)')
+parser.add_argument('-t', dest='targets', help='Path to target search mzml file')
+parser.add_argument('-d', dest='decoys', help='Path to decoy search mzml file')
 parser.add_argument('-p', dest='pin', help='Path to pin file')
 
 args = parser.parse_args()
 
-# parse mzid file: xmltodict imports it as a dictionary
-with open(args.mzml) as fd:
-     doc = xmltodict.parse(fd.read())
-
-mapper = {}
-
-# going through each SpectrumIdentificationResult. There may be more than one hit. "mapper" will correspond each perc_id with the mgf file "TITLE" for the spectrum
-for i in range(len(doc['MzIdentML']['DataCollection']['AnalysisData']['SpectrumIdentificationList']['SpectrumIdentificationResult'])):
-    if type(doc['MzIdentML']['DataCollection']['AnalysisData']['SpectrumIdentificationList']['SpectrumIdentificationResult'][i]['SpectrumIdentificationItem']) is list:
-        for j in range(len(doc['MzIdentML']['DataCollection']['AnalysisData']['SpectrumIdentificationList']['SpectrumIdentificationResult'][i]['SpectrumIdentificationItem'])):
+def get_indices(doc):
+    """
+    Given a dictionary with the .mzml file, go through every
+    SpectrumIdentificationResult and match the mgf TITLE to the initial part of
+    the Percolator index. Save these correspondences in a dictionary
+    """
+    mapper = {}
+    for i in range(len(doc['MzIdentML']['DataCollection']['AnalysisData']['SpectrumIdentificationList']['SpectrumIdentificationResult'])):
+        if type(doc['MzIdentML']['DataCollection']['AnalysisData']['SpectrumIdentificationList']['SpectrumIdentificationResult'][i]['SpectrumIdentificationItem']) is list:
+            for j in range(len(doc['MzIdentML']['DataCollection']['AnalysisData']['SpectrumIdentificationList']['SpectrumIdentificationResult'][i]['SpectrumIdentificationItem'])):
+                spectrum = doc['MzIdentML']['DataCollection']['AnalysisData']['SpectrumIdentificationList']['SpectrumIdentificationResult'][i]
+                hit = spectrum['SpectrumIdentificationItem'][j]
+                # perc_id = hit['@id'] + '_' + hit['@chargeState'] + '_' + hit['@rank']
+                perc_id = hit['@id']
+                title = spectrum['cvParam']['@value']
+                mapper[perc_id] = title
+        else:
             spectrum = doc['MzIdentML']['DataCollection']['AnalysisData']['SpectrumIdentificationList']['SpectrumIdentificationResult'][i]
-            hit = spectrum['SpectrumIdentificationItem'][j]
+            hit = spectrum['SpectrumIdentificationItem']
             # perc_id = hit['@id'] + '_' + hit['@chargeState'] + '_' + hit['@rank']
             perc_id = hit['@id']
             title = spectrum['cvParam']['@value']
             mapper[perc_id] = title
-    else:
-        spectrum = doc['MzIdentML']['DataCollection']['AnalysisData']['SpectrumIdentificationList']['SpectrumIdentificationResult'][i]
-        hit = spectrum['SpectrumIdentificationItem']
-        # perc_id = hit['@id'] + '_' + hit['@chargeState'] + '_' + hit['@rank']
-        perc_id = hit['@id']
-        title = spectrum['cvParam']['@value']
-        mapper[perc_id] = title
 
-# open percolator features; add column with mgf title
+    return mapper
 
 def lazy_pin_parser(path):
     """
@@ -57,14 +60,68 @@ def lazy_pin_parser(path):
             data.loc[i-1] = tmp
     return data
 
+# open percolator features; add column with mgf title
+sys.stdout.write('Parsing pin file... ')
 pin = lazy_pin_parser(args.pin)
-# Adding mgf "TITLE" column
 pin['TITLE'] = [None] * len(pin)
-for i in range(1, len(pin)+1): # because index starts at 1
-    k = '_'.join(pin.loc[i, 'SpecId'].split('_')[-6:-3])
-    if k in mapper.keys():
-        pin.loc[i, 'TITLE'] = mapper[k]
-    else:
-        continue
+sys.stdout.write('Done! \n')
+# parse mzid file: xmltodict imports it as a dictionary
+# concatenated searches yield one mzml
+if args.mzml:
+    sys.stdout.write('Concatenated search results; parsing .mzml... ')
+    with open(args.mzml) as fd:
+         doc = xmltodict.parse(fd.read())
 
+    # Adding mgf "TITLE" column
+    mapper = get_indices(doc)
+    sys.stdout.write('Done! \n')
+
+    sys.stdout.write('Adding "TITLE" to pin file... ')
+    for i in range(1, len(pin)+1): # because index starts at 1
+        k = '_'.join(pin.loc[i, 'SpecId'].split('_')[-6:-3])
+        if k in mapper.keys():
+            pin.loc[i, 'TITLE'] = mapper[k]
+        else:
+            continue
+    sys.stdout.write('Done! \n')
+
+# for separate target-decoy there are two mzml
+elif args.targets and args.decoys:
+    sys.stdout.write('Separate target and decoy search; \n')
+
+    sys.stdout.write('parsing targets .mzml... ')
+    with open(args.targets) as fd:
+         doc = xmltodict.parse(fd.read())
+
+    mapper_targets = get_indices(doc)
+    sys.stdout.write('Done! \n')
+
+    sys.stdout.write('parsing decoys .mzml... ')
+    with open(args.decoys) as fd:
+         doc = xmltodict.parse(fd.read())
+
+    mapper_decoys = get_indices(doc)
+    sys.stdout.write('Done! \n')
+
+    sys.stdout.write('Adding "TITLE" to pin file... ')
+    for i in range(1, len(pin)+1): # because index starts at 1
+        k = '_'.join(pin.loc[i, 'SpecId'].split('_')[-6:-3])
+        if pin.loc[i, 'Label'] == "-1":
+            if k in mapper_decoys.keys():
+                pin.loc[i, 'TITLE'] = mapper_decoys[k]
+            else:
+                sys.stdout.write('oops\n')
+                continue
+        elif pin.loc[i, 'Label'] == "1":
+            if k in mapper_targets.keys():
+                pin.loc[i, 'TITLE'] = mapper_targets[k]
+            else:
+                sys.stdout.write('oops\n')
+                continue
+
+    sys.stdout.write('Done! \n')
+
+
+sys.stdout.write('Saving pin file... ')
 pin.to_csv(args.pin, sep='\t', index=False)
+sys.stdout.write('Done! \n')
