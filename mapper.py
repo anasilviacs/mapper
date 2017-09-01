4e310fed2c114f60c752a157b6004d6e05e6fc59
anasilviacs <anascsilva@vib-ugent.be>
2017-09-01 10:50:16 +0200

    Msgs not showing up; flushing stdout

--- mapper.py
+++ mapper.py
@@ -63,21 +63,26 @@ if __name__ == "__main__":
 
     # open percolator features; add column with mgf title
     sys.stdout.write('Parsing pin file... ')
+    sys.stdout.flush()
     pin = lazy_pin_parser(args.pin)
     pin['TITLE'] = [None] * len(pin)
     sys.stdout.write('Done! \n')
+    sys.stdout.flush()
     # parse mzid file: xmltodict imports it as a dictionary
     # concatenated searches yield one mzml
     if args.mzml:
         sys.stdout.write('Concatenated search results; parsing .mzml... ')
+        sys.stdout.flush()
         with open(args.mzml) as fd:
              doc = xmltodict.parse(fd.read())
 
         # Adding mgf "TITLE" column
         mapper = get_indices(doc)
         sys.stdout.write('Done! \n')
+        sys.stdout.flush()
 
         sys.stdout.write('Adding "TITLE" to pin file... ')
+        sys.stdout.flush()
         for i in range(1, len(pin)+1): # because index starts at 1
             k = '_'.join(pin.loc[i, 'SpecId'].split('_')[-6:-3])
             if k in mapper.keys():
@@ -85,26 +90,33 @@ if __name__ == "__main__":
             else:
                 continue
         sys.stdout.write('Done! \n')
+        sys.stdout.flush()
 
     # for separate target-decoy there are two mzml
     elif args.targets and args.decoys:
         sys.stdout.write('Separate target and decoy search; \n')
+        sys.stdout.flush()
 
         sys.stdout.write('parsing targets .mzml... ')
+        sys.stdout.flush()
         with open(args.targets) as fd:
              doc = xmltodict.parse(fd.read())
 
         mapper_targets = get_indices(doc)
         sys.stdout.write('Done! \n')
+        sys.stdout.flush()
 
         sys.stdout.write('parsing decoys .mzml... ')
+        sys.stdout.flush()
         with open(args.decoys) as fd:
              doc = xmltodict.parse(fd.read())
 
         mapper_decoys = get_indices(doc)
         sys.stdout.write('Done! \n')
+        sys.stdout.flush()
 
         sys.stdout.write('Adding "TITLE" to pin file... ')
+        sys.stdout.flush()
         for i in range(1, len(pin)+1): # because index starts at 1
             k = '_'.join(pin.loc[i, 'SpecId'].split('_')[-6:-3])
             if pin.loc[i, 'Label'] == "-1":
@@ -112,17 +124,22 @@ if __name__ == "__main__":
                     pin.loc[i, 'TITLE'] = mapper_decoys[k]
                 else:
                     sys.stdout.write('oops\n')
+                    sys.stdout.flush()
                     continue
             elif pin.loc[i, 'Label'] == "1":
                 if k in mapper_targets.keys():
                     pin.loc[i, 'TITLE'] = mapper_targets[k]
                 else:
                     sys.stdout.write('oops\n')
+                    sys.stdout.flush()
                     continue
 
         sys.stdout.write('Done! \n')
+        sys.stdout.flush()
 
 
     sys.stdout.write('Saving pin file... ')
+    sys.stdout.flush()
     pin.to_csv(args.pin, sep='\t', index=False)
     sys.stdout.write('Done! \n')
+    sys.stdout.flush()
